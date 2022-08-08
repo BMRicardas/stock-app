4f95538d7338792034c389a9f30ab8a19d2291ca
BMRicardas <Ricardas.Brazdzius@gmail.com>
2022-08-08 22:33:32 +0300

    create api config

---
+++ src/config/config.ts
@@ -0,0 +1,8 @@
+export const config = {
+  baseURL: 'http://localhost:4000',
+  endpoints: {
+    profile: '/stock/profile2',
+    candle: '/stock/candle',
+  },
+  token: 'cblb0riad3i4fg7gu1j0',
+};
